2e52666d776f35f13f54b25c657e44ca122cca91
Fangyin Cheng <staneyffer@gmail.com>
2025-04-16 09:38:41 +0800

    Update llama.cpp submodule to latest version

--- scripts/apply-patch.sh
+++ scripts/apply-patch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-pushd llama-cpp-server-py-core/llama.cpp
+echo "✅ No patches to apply"
+
+# pushd llama-cpp-server-py-core/llama.cpp
 
 # Apply patch
-git apply ../../patches/llama.cpp.patch
+# git apply ../../patches/llama.cpp.patch
 
-popd
-echo "✅ Patch applied successfully"
+# popd
+# echo "✅ Patch applied successfully"
